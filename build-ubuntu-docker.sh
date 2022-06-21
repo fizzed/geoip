9f1bc726d230053fcc168cfc4ce20d15c26c3fc7
Joe Lauer <joe@lauer.bz>
2022-06-21 15:59:50 -0400

    Make docker ubuntu build support aarch64

--- build-ubuntu-docker.sh
+++ build-ubuntu-docker.sh
@@ -1,1 +1,8 @@
-docker build --file Dockerfile.ubuntu --tag graalvm-ubuntu .
+ARCH=$(uname -p)
+GRAALVM_ARCH="amd64"
+
+if [ "$ARCH" -eq "aarch64" ]; then
+  GRAALVM_ARCH="aarch64"
+fi
+
+docker build --build-arg GRAALVM_ARCH=$GRAALVM_ARCH --file Dockerfile.ubuntu --tag graalvm-ubuntu .
