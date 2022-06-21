2ea62eef671f9830b3dbe53e99757a7700f33d07
Joe Lauer <joe@lauer.bz>
2022-06-21 20:25:45 +0000

    Improve build process for ubuntu

--- build-ubuntu-docker.sh
+++ build-ubuntu-docker.sh
@@ -1,8 +1,11 @@
 ARCH=$(uname -p)
 GRAALVM_ARCH="amd64"
 
-if [ "$ARCH" -eq "aarch64" ]; then
+echo "Detected arch $ARCH"
+
+if [[ $ARCH == "aarch64" ]]; then
   GRAALVM_ARCH="aarch64"
+  echo "Updating arch to aarch64!"
 fi
 
 docker build --build-arg GRAALVM_ARCH=$GRAALVM_ARCH --file Dockerfile.ubuntu --tag graalvm-ubuntu .
