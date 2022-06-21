cff5bd085d6fe275aea747c4b904f8b1883721e4
Joe Lauer <joe@lauer.bz>
2022-06-21 16:38:53 -0400

    Build files for alpine aarch64

--- build-alpine-docker.sh
+++ build-alpine-docker.sh
@@ -1,1 +1,11 @@
-docker build --file Dockerfile.alpine --tag graalvm-alpine .
+ARCH=$(uname -p)
+GRAALVM_ARCH="amd64"
+
+echo "Detected arch $ARCH"
+
+if [[ $ARCH == "aarch64" ]]; then
+  GRAALVM_ARCH="aarch64"
+  echo "Updating arch to aarch64!"
+fi
+
+docker build --build-arg GRAALVM_ARCH=$GRAALVM_ARCH --file Dockerfile.alpine --tag graalvm-alpine .
--- build-alpine-native-image.sh
+++ build-alpine-native-image.sh
@@ -4,4 +4,10 @@ mvn clean
 
 docker run -it --rm --workdir /project --user "$(id -u):$(id -g)" --volume ${PWD}:/project graalvm-alpine sh --login -c "mvn -Pnative package"
 
-mv target/geoip-server-linux-x86_64 target/geoip-server-alpine-x86_64
+if [ -f "target/geoip-server-linux-aarch_64" ]; then
+  mv -f target/geoip-server-linux-aarch_64 target/geoip-server-alpine-aarch64
+fi
+
+if [ -f "target/geoip-server-linux-x86_64" ]; then
+  mv -f target/geoip-server-linux-x86_64 target/geoip-server-alpine-x86_64
+fi
